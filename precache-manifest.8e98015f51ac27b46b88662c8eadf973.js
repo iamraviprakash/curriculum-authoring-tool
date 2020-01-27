0399538b1519871ae132687047091dd76b76cf76
Ravi Prakash <raviprakash.coder@gmail.com>
2020-01-27 23:13:22 +0530

    Updates

---
+++ precache-manifest.8e98015f51ac27b46b88662c8eadf973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae82c43028e871c422db13bda23b41ee",
+    "url": "/curriculum-authoring-tool/index.html"
+  },
+  {
+    "revision": "76ce128f589f951e7c8c",
+    "url": "/curriculum-authoring-tool/static/css/main.991120f4.chunk.css"
+  },
+  {
+    "revision": "e6deed8abd559a503d26",
+    "url": "/curriculum-authoring-tool/static/js/2.49f2408b.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/curriculum-authoring-tool/static/js/2.49f2408b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76ce128f589f951e7c8c",
+    "url": "/curriculum-authoring-tool/static/js/main.e8052867.chunk.js"
+  },
+  {
+    "revision": "74b29c8ec3ef89704285",
+    "url": "/curriculum-authoring-tool/static/js/runtime-main.79e6b076.js"
+  }
+]);
